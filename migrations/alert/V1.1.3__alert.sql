f3de0843ef6e57decda3dbaf71e69511d2e6c3b5
Anitha Singamsetti <Anitha.Singamsetti@elysiumanalytics.ai>
2022-05-06 22:23:33 +0530

    sakshtam test case 7

---
+++ migrations/alert/V1.1.3__alert.sql
@@ -0,0 +1,3 @@
+use SCHEMA ci_cd_schema;
+alter TABLE  saksham_alert_test1 add column info3 variant; 
+alter TABLE  saksham_alert_test1 add column info4 variant; 
---
+++ migrations/profiles/V1.1.2__profile.sql
@@ -0,0 +1,3 @@
+use SCHEMA ci_cd_schema;
+alter TABLE  saksham_profile_test1 add column info3 variant; 
+alter TABLE  saksham_profile_test1 add column info4 variant; 
